033072bf309d2425b2bc73a205f6327c0eccde29
Jonatan Magnusson <jonatan@myeden.se>
2018-02-07 23:22:09 +0100

    Adjusted eslinter settings

--- .eslintrc.js
+++ .eslintrc.js
@@ -6,6 +6,10 @@ module.exports = {
     "rules": {
         "indent": ["error", 4],
         "no-plusplus": 0,
-        "no-mixed-operators": 0
+        "no-mixed-operators": 0,
+        "class-methods-use-this": 0,
+        "no-param-reassign": 0,
+        "no-console": 0,
+        "comma-dangle": 0
     }
 };
